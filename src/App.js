2776c6c1b0089e4db73122aaa9b59e5912c2872a
willson <93517421+willson1661@users.noreply.github.com>
2023-11-11 18:53:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import ScrollButton from "../src/ScrollButton";
+import { Content, Heading } from "../src/Styles";
+
+function App() {
+  return (
+    <Fragment>
+      <Heading>dsdcsdcsdcsdcdcs</Heading>
+      <Content />
+      <ScrollButton />
+    </Fragment>
+  );
+}
+
+export default App;
